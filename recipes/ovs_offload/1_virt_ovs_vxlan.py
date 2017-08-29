roidayan/lnst-hw-offload-config

commit 616a1aadb15d5224a2062dff24712cfb55a37753

lnst: 1_virt_ovs_vxlan.py: sleep a second before testing ping
Signed-off-by: Roi Dayan <roid@mellanox.com>

recipes/ovs_offload/1_virt_ovs_vxlan.py
@@ -69,4 +69,6 @@ def verify_tc_rules(proto):
         tl.custom(host1, desc, 'ERROR: cannot find tc rule')
 
 
+# sleep a second before testing.
+ctl.wait(1)
 do_pings()

commit d0c28687140c2e330200b0881d7cce0d4b5b3a74

lnst: 1_virt_ovs_vxlan.py: increase wait before test to 3 seconds
1-2 seconds were not enough and failed in 10 iterations check.
3 seconds passed all 10 iterations.

Fixes: 616a1aadb15d ("lnst: 1_virt_ovs_vxlan.py: sleep a second before testing ping")
Signed-off-by: Roi Dayan <roid@mellanox.com>

recipes/ovs_offload/1_virt_ovs_vxlan.py
@@ -70,5 +70,5 @@ def verify_tc_rules(proto):
 
 
 # sleep a second before testing.
-ctl.wait(1)
+ctl.wait(3)
 do_pings()